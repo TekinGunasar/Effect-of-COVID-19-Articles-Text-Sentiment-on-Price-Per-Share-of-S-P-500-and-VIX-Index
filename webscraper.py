7c90e9be09311892a9a4208cf63b59e9a2d03cba
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2020-12-13 05:06:35 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 #Mar 1, 2020 to Oct 27,2020
 
-url_stem = "https://www.wsj.com/news/archive/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0 '}
 
 urls = [line.split('\n')[0] for line in open("page_urls.txt",'r').readlines()]
@@ -15,11 +14,25 @@ keywords = ["covid","coronavirus","ventilator","virus","covid","covid-19","cases
             "distancing","mask","face-masks","masks","lockdown"]
 
 for url in urls:
+    print(url)
     date_split = url.split("/")
     year = month = date_split[len(date_split)-3]
     month = date_split[len(date_split)-2]
     day = date_split[len(date_split)-1].split("?")[0]
-    f = open("covid_articles/" + year + "-" + month + "-" + day + ".txt",'w+')
+    f = open("covid_articles/" + year + "-" + month + "-" + day + ".txt",'w')
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.content, 'lxml')
+    links = soup.find_all("a")
+    for link in links:
+        try:
+            if len(link.attrs['class']) == 0:
+                article_title = link.text.lower()
+                for word in article_title.split(" "):
+                    if word in keywords:
+                        f.write(link.attrs['href']+'\n')
+                        break
+        except:
+            pass
 
 
 
