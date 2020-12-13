59644c086f5dd2297b9348959ddae1178f99d7ae
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2020-12-13 04:07:38 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -7,23 +7,20 @@ import pandas as pd
 
 url_stem = "https://www.wsj.com/news/archive/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0 '}
-article_urls = []
 
-start_date = datetime(2020,3,1)
-periods=240
+urls = [line.split('\n')[0] for line in open("article_urls.txt",'r').readlines()]
 
-date_list = [str(date).split(" ")[0].replace("-","/")
-            for date in pd.date_range(start_date, periods=periods).tolist()]
+test_url = urls[0]
+result = requests.get(test_url,headers=headers)
+src = result.content
+soup = BeautifulSoup(src,'lxml')
+links = soup.find_all("a")
 
-for date in date_list:
-    cur_url = url_stem+date
-    page_number = 1
-    while(requests.get(cur_url+"?page="+str(page_number),headers=headers).status_code == 200 and page_number<=2):
-        article_urls.append(cur_url+"?page="+str(page_number))
-        print(cur_url+"?page="+str(page_number))
-        page_number+=1
+for link in links:
+    try:
+        if len(link.attrs['class']) == 0:
+            print(link.attrs['href'])
+    except KeyError:
+        pass
 
-f = open("article_urls.txt",'w')
-for url in article_urls:
-    f.write(url + "\n")
 
