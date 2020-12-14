7a5e62aadd89b720aa7c161255fa2b72f6b01565
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2020-12-13 19:47:51 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,32 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import pandas as pd
 import os
-from pathlib import Path
 
+#Mar 1, 2020 to Oct 27,2020
+url_stem = "https://www.wsj.com/news/archive/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0 '}
 
-dir = "covid_articles_urls"
-for file in os.listdir(dir):
-    cur_date = Path('covid_articles_urls/'+file).stem
-    print(cur_date)
-    cur_set_of_urls = open(dir + "/" + file,'r').readlines()
-    f = open("covid_articles_summaries/" + cur_date + ".txt.txt",'w')
+for file in os.listdir("covid_articles_urls"):
+    print(file)
+    cur_set_of_urls = open("covid_articles_urls/" + file,'r').readlines()
+    f = open("covid_articles_urls/" + file,'w')
+    open("covid_articles_urls/" + file,'w').close()
     for url in cur_set_of_urls:
-        try:
-            if requests.get(url.strip('\n'),headers=headers).status_code == 200:
-                src = requests.get(url.strip('\n'),headers=headers).content
-                soup = BeautifulSoup(src,'lxml')
-                meta_tags = soup.find_all('meta')
-                for tag in meta_tags:
-                    try:
-                       if tag.attrs["name"] == "article.summary":
-                           f.write(tag.attrs["content"] + '\n')
-                           break
-                    except KeyError:
-                        pass
-        except requests.exceptions.MissingSchema:
-            pass
-
-
-
+        if url.startswith("https://"):
+            f.write(url)
 
