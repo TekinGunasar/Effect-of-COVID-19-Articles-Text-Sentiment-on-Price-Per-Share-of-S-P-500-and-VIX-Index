d8aaa7e79b27dcda625f728af4ee29e1c3a4da33
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2020-12-18 02:34:51 -0800

    Update csv_parser.py

--- csv_parser.py
+++ csv_parser.py
@@ -1,32 +1,17 @@
 GSPC_lines = open("^GSPC.csv",'r').readlines()
 VIX_lines = open("^VIX.csv",'r').readlines()
+sentiments = open("text_sentiment_per_day.txt").readlines()
 
+used_sentiments = []
+VIX_price_changes = []
+GSPC_price_changes = []
 
-available_dates_GSPC = []
-available_dates_VIX = []
+VIX_dates = []
+GSPC_dates = []
 
-GSPC_prices = []
-VIX_prices = []
+f = open("sentiment_and_price_change.txt",'w')
 
-sentiments = open("text_sentiment_per_day.txt",'r').readlines()
-
-for line in VIX_lines:
-    cur_line = line.split(",")
-    VIX_prices.append(cur_line[4])
-    available_dates_VIX.append(cur_line[0])
-
-f_VIX = open("sentiment_day_and_price_VIX.txt.txt",'w')
-
-for sentiment in sentiments:
-    cur_sentiment = sentiment.split(" ")
-    for i in range(len(available_dates_VIX)):
-        if available_dates_VIX[i] == cur_sentiment[0]:
-            used_sentiment = cur_sentiment[len(cur_sentiment)-1]
-            used_price = VIX_prices[i]
-            res_list = [cur_sentiment[0],str(used_sentiment),str(used_price)]
-            joined_res_list = ",".join(res_list)
-            f_VIX.write(joined_res_list + "\n\n")
-
+#getting available dates for GSPC
 for line in GSPC_lines:
     try:
         cur_line = line.split(',')
@@ -35,34 +20,31 @@ for line in GSPC_lines:
         cur_date[len(cur_date)-1] = cur_date[0]
         cur_date[0] = temp
         formatted_date = cur_date[0] + "-" + cur_date[1] + "-" + cur_date[2]
-        GSPC_prices.append(cur_line[4])
-        available_dates_GSPC.append(formatted_date)
+        GSPC_dates.append(formatted_date)
     except IndexError:
         pass
 
-f_GSPC = open("sentiment_day_and_price_GSPC.txt.txt",'w')
+#get available VIX dates
+for line in VIX_lines:
+    VIX_dates.append(line.split(",")[0])
 
-for sentiment in sentiments:
-    cur_sentiment = sentiment.split(" ")
-    cur_date_splitted = cur_sentiment[0].split("-")
-    formatted_month = ""
-    formatted_day = ""
-    if int(cur_date_splitted[1][0]) == 0:
-        formatted_month = cur_date_splitted[1][1]
-    else:
-        formatted_month = cur_date_splitted[1]
+#get price changes for VIX and GSPC and get associated sentiments
+for i in range(len(GSPC_lines)-1):
+    GSPC_price_changes.append(float(GSPC_lines[i+1].split(",")[4]) - float(GSPC_lines[i].split(",")[4]))
 
-    if int(cur_date_splitted[2][0]) == 0:
-        formatted_day = cur_date_splitted[2][1]
-    else:
-        formatted_day = cur_date_splitted[2]
+for i in range(len(VIX_lines)-1):
+    VIX_price_changes.append(float(VIX_lines[i+1].split(",")[4]) - float(VIX_lines[i].split(",")[4]))
 
-    formatted_date = str(2020) + "-" + formatted_month + "-" + formatted_day
-    for i in range(len(available_dates_GSPC)):
-        if available_dates_GSPC[i] == formatted_date:
-            used_sentiment = cur_sentiment[len(cur_sentiment)-1]
-            price = GSPC_prices[i]
-            res_list = [str(formatted_date),str(used_sentiment),str(price)]
-            joined_res_list = ",".join(res_list)
-            f_GSPC.write(joined_res_list+'\n\n')
+for i in range(len(sentiments)-1):
+    if sentiments[i].split(" ")[0] in VIX_dates:
+        used_sentiments.append(sentiments[i].split(" ")[2])
+
+
+
+#write tofile
+for i in range(len(used_sentiments)):
+    entry = tuple((float(used_sentiments[i]),VIX_price_changes[i],GSPC_price_changes[i]))
+    f.write((str(entry).strip("(").strip(")")).replace(" ","") + '\n')
+
+
 
